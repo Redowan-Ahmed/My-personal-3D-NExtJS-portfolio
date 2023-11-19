5dcf065766169915402b56763142cc1236bc6741
Redowan Ahmed <55311231+Redowan-Ahmed@users.noreply.github.com>
2023-11-19 19:33:19 +0600

    unWorking

--- portfolio/src/app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-
-function page() {
-  return (
-    <section>Contact</section>
-  )
-}
-
-export default page
--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,14 +1,33 @@
-import Image from 'next/image'
-import { Canvas } from ''
+"use client";
+import { Suspense, useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import { Canvas } from "@react-three/fiber";
+import Loader from "@/components/Loader";
+import { Island } from "@/models/Island";
+
 export default function Home() {
   return (
     <main className="w-full h-screen relative">
-      {/* <div className='absolute top-28 left-0 right-0 z-10 flex justify-center items-center'>
+      {/*  <div className='absolute top-28 left-0 right-0 z-10 flex justify-center items-center'>
         popup
-      </div> */}
+      </div>  */}
 
-
-
+      <div id="canvas-container w-full h-screen bg-transparent">
+        <Canvas
+          frameloop="demand"
+          className="w-full h-screen bg-transparent"
+          camera={{ near: 0.1, far: 1000 }}
+        >
+          <Suspense fallback={<Loader />}>
+            <directionalLight />
+            <ambientLight intensity={0.5} />
+            <pointLight position={[3, 6, -4]} />
+            <spotLight />
+            <hemisphereLight />
+            <Island/>
+          </Suspense>
+        </Canvas>
+      </div>
     </main>
-  )
+  );
 }
